4cbdb2c55aa1dffd6d8ded99a1ea48312dedffa5
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2024-12-01 15:07:43 +0900

    Update the header file for this library - Add `IsUniformRandomBitGenerator`

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ C++ implementation of Xorshift using SIMD (emmintrin &amp; immintrin)
 ## Reference
 
 - [std::uniform_random_bit_generator - cppreference.com](https://en.cppreference.com/w/cpp/numeric/random/uniform_random_bit_generator)
+- [uniform_random_bit_generator - cpprefjp C++日本語リファレンス](https://cpprefjp.github.io/reference/random/uniform_random_bit_generator.html)
 - [Intel® Intrinsics Guide](https://www.intel.com/content/www/us/en/docs/intrinsics-guide/index.html)
 
 <!-- EOF -->
--- SimdXorshift.hpp
+++ SimdXorshift.hpp
@@ -1,2 +1,30 @@
 /// @file SimdXorshift.hpp
 /// @brief C++ implementation of Xorshift using SIMD (emmintrin & immintrin)
+
+#include <random>
+
+namespace simd_xorshift
+{
+    /// @brief Checks if the given type is `std::uniform_random_bit_generator`
+    /// @tparam G A type that should satisfy the `std::uniform_random_bit_generator` concept
+    /// @return `true` if `G` is `std::uniform_random_bit_generator`.
+    /// @note C++20 must be required for this function to work
+    /// @note https://cpprefjp.github.io/reference/random/uniform_random_bit_generator.html
+    template<std::uniform_random_bit_generator G>
+    static constexpr bool IsUniformRandomBitGenerator(void)
+    {
+        return true;
+    }
+
+
+    /// @brief Checks if the given type is `std::uniform_random_bit_generator`
+    /// @tparam G A type that should satisfy the `std::uniform_random_bit_generator` concept
+    /// @return `false` if `G` is NOT `std::uniform_random_bit_generator`.
+    /// @note C++20 must be required for this function to work
+    /// @note https://cpprefjp.github.io/reference/random/uniform_random_bit_generator.html
+    template<typename G>
+    static constexpr bool IsUniformRandomBitGenerator(void)
+    {
+        return false;
+    }
+}
